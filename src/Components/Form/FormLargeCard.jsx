30cd6cfe3b6d04134cad16688388d1823b2bca73
Lidus1 <lulupacanda@seznam.cz>
2024-04-16 19:56:43 +0200

    lets do thiiis

--- src/Components/Form/FormLargeCard.jsx
+++ src/Components/Form/FormLargeCard.jsx
@@ -1,11 +1,13 @@
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { FormRolesCard } from './FormRolesCard'
 import { FormRawCard } from './FormRawCard'
+import { FormMediumCard } from './FormMediumCard'
 
-export const FormLargeCard = ({form}) => {
+export const FormLargeCard = ({form, children}) => {
     return (
-        <CardCapsule title={"Fromular"}>
+        <CardCapsule title={"Formular " + form?.id}>
         {/* <Row>
             <Col md={3}>
                 <FormMediumCard form={form}/>
