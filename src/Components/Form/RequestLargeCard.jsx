47b3e36ffba21fcb20a9cfe25596c7cb268306cd
Lidus1 <lulupacanda@seznam.cz>
2024-05-09 14:51:51 +0200

    request

---
+++ src/Components/Form/RequestLargeCard.jsx
@@ -0,0 +1,50 @@
+import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+// import { RequestRolesCard } from './RequestRolesCard'
+import { RequestRawCard } from './RequestRawCard'
+import { RequestCard } from './RequestCard'
+// import { RequestMediumCard } from './RequestMediumCard'
+
+export const RequestLargeCard = ({request, children}) => {
+    return (
+        <CardCapsule title={"Žádost "}>
+        {/* <Row>
+            <Col md={3}>
+                <RequestMediumCard request={request}/>
+            </Col>
+            <Col md={6}>
+                {children}
+            </Col>
+            <Col md={3}>
+                <RequestRolesCard request={request}/>
+            </Col>
+            
+        </Row>
+        <br /> */}
+        <Row>
+           <Col md = {3}> {/* basic info */}
+                <RequestCard request={request}/>
+            </Col>
+
+            <Col md = {3}>{/* section */}
+                
+            </Col>
+
+            <Col md = {3}>{/* part */}
+                
+            </Col>
+
+            <Col md = {3}>{/* item */}
+                
+            </Col>
+        </Row>
+        <Row>
+            <Col>{/* raw */}
+                <RequestRawCard request={request}/>
+            </Col>
+        </Row>
+    </CardCapsule>
+
+    )
+}
--- src/Pages/FormRequestPage.jsx
+++ src/Pages/FormRequestPage.jsx
@@ -1,19 +1,19 @@
 // import { useFreshItem } from "@hrbolek/uoisfrontend-shared/src"
 import { useParams } from "react-router-dom"
-import { FormLargeCard } from "../Components"
+import { RequestLargeCard } from "../Components"
 import { useFreshItem, CreateAsyncQueryValidator, useDispatch } from "@hrbolek/uoisfrontend-shared/src"
-import { FetchFormByIdAsyncAction } from "../Queries"
+import { FetchRequestByIdAsyncAction } from "../Queries"
 
 const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst uživatele", success: "Načtení uživatele se povedlo"})
-export const FormPageRequest = ()  => {
+export const RequestPageRequest = ()  => {
     const {id} = useParams()
     const [onResolve, onReject] = validator(useDispatch())
-    const [form, formPromise] = useFreshItem({id}, FetchFormByIdAsyncAction)
-    formPromise.then(onResolve, onReject)
+    const [request, requestPromise] = useFreshItem({id}, FetchRequestByIdAsyncAction)
+    requestPromise.then(onResolve, onReject)
 
-    if (form) {
+    if (request) {
         return (
-            <FormLargeCard form={form} />
+            <RequestLargeCard request={request} />
         )
     } else {
         return (
