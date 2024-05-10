7d6730d07b8e9a5f086388b896743e07b6a66b88
nikdo3 <tatanek2003@gmail.com>
2024-05-10 09:03:13 +0200

    test FormSections

--- src/Components/Form/FormSections.jsx
+++ src/Components/Form/FormSections.jsx
@@ -1,9 +1,15 @@
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+// stránka z form načte pole se sekcemi a pro každou sekci vytvoří CardCapsule ve kterém bude název sekce
 export const FormSections = ({form}) => {
     return (
-        <p>lol</p>
+        <Row>
+            {form.sections.map((section, index) => (
+                <Col key={index} xs={12} sm={6} md={4} lg={3}>
+                    <CardCapsule title={section.name} />
+                </Col>
+            ))}
+        </Row>
     )
 }
