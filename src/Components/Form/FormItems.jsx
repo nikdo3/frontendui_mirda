29a5274575934901259d567447ce7fb2ce225db4
nikdo3 <miroslavjiri.cani@unob.cz>
2024-05-15 14:22:06 +0200

    push Ludmile na hraní

--- src/Components/Form/FormItems.jsx
+++ src/Components/Form/FormItems.jsx
@@ -1,6 +1,7 @@
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { FormUserCard } from './FormUserCard'
 
 export const FormItems = ({part}) => {
     const items = part?.items || []
@@ -8,8 +9,15 @@ export const FormItems = ({part}) => {
         <Row>
             {items.map((item) => (
                 <Row key={item.id} >
-                    <Col>{item.name}:</Col>
-                    <Col>{item.value}</Col>
+                    
+                    {item.type?.name === 'student' ? (
+                        <FormUserCard value={item.value} />
+                    ) : (
+                        <>
+                            <Col>{item.name}:</Col>
+                            <Col>{item.value}</Col>
+                        </>
+                    )}
                 </Row>
             ))}
         </Row>
