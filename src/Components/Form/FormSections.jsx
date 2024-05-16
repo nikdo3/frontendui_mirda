121c3aa606174734cb509c2557b33b0418faca38
nikdo3 <miroslavjiri.cani@unob.cz>
2024-05-16 20:48:30 +0200

    tlačím

--- src/Components/Form/FormSections.jsx
+++ src/Components/Form/FormSections.jsx
@@ -1,6 +1,7 @@
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { FormParts } from './FormParts'
 
 // stránka z form načte pole se sekcemi a pro každou sekci vytvoří CardCapsule ve kterém bude název sekce
 
@@ -11,7 +12,7 @@ export const FormSections = ({form}) => {
             {sections.map((section, index) => (
                 <Col key={section.id} xs={12} sm={6} md={4} lg={3}>
                     <CardCapsule title={section.name}>
-                        
+                        <FormParts section={section} />
                     </CardCapsule>
                 </Col>
             ))}
