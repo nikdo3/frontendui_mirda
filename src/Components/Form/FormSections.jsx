c787b2a815dd5dac6d4fed2b48c7dfc73b10476b
Lidus1 <lulupacanda@seznam.cz>
2024-05-16 20:32:51 +0200

    hmm

--- src/Components/Form/FormSections.jsx
+++ src/Components/Form/FormSections.jsx
@@ -1,18 +1,21 @@
 import { CardCapsule } from '@hrbolek/uoisfrontend-shared/src'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { FormParts } from './FormParts'
+
 // stránka z form načte pole se sekcemi a pro každou sekci vytvoří CardCapsule ve kterém bude název sekce
 
 export const FormSections = ({form}) => {
     const sections = form?.sections || []
     return (
         <Row>
-            {sections.map((section) => (
-                    <CardCapsule key={section.id} title={section.name}>
-                        <FormParts section={section}/>
+            {sections.map((section, index) => (
+                <Col key={section.id} xs={12} sm={6} md={4} lg={3}>
+                    <CardCapsule title={section.name}>
+                        
                     </CardCapsule>
+                </Col>
             ))}
         </Row>
     )
 }
+
