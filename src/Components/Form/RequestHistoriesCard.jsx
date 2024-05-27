abcee1aa5bb40937c5c0e7a4d244ee88ae0d5c86
Lidus1 <lulupacanda@seznam.cz>
2024-05-27 12:23:58 +0200

    request

--- src/Components/Form/RequestHistoriesCard.jsx
+++ src/Components/Form/RequestHistoriesCard.jsx
@@ -1,35 +1,37 @@
 import { Dropdown } from "react-bootstrap";
 import { CardCapsule } from "@hrbolek/uoisfrontend-shared/src";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 
-export const RequestHistoriesCard = ({ request,children,menu=true }) => { // eslint-disable-line react/prop-types
+
+
+
+export const RequestHistoriesCard = ({ request,children, table}) => { // eslint-disable-line react/prop-types
   return (
     <div>
       <CardCapsule title="Historie">
-        <Row>
-          <Col>Historie</Col>
-          <Col>
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                {menu ? "Vyberte" : "Zobrazit"}
-              </Dropdown.Toggle>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Datum</th>
+              <th>Kdo</th>
+              <th>Poznámka</th>
+            </tr>
+          </thead>
+          <tbody>
+            {request?.histories?.map((requesty, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{request?.created}</td>
+                <td>{"c"}</td>
+                <td>{"d"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+       
 
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">a</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">b</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">c</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Col>
-        </Row>
-        <Row>
-          <Col>Historie</Col>
-          <Col>
-            <p>obsah</p>
-          </Col>
-        </Row>
+        
       </CardCapsule>
     </div>
   );
