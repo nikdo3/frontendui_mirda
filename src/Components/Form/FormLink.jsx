3801ef23ebe0f54190c47dd778b2c1fb508b8d83
Lidus1 <lulupacanda@seznam.cz>
2024-07-04 15:39:48 +0200

    přidán komentář

--- src/Components/Form/FormLink.jsx
+++ src/Components/Form/FormLink.jsx
@@ -1,5 +1,14 @@
 import {ProxyLink} from '@hrbolek/uoisfrontend-shared/src'
 
+/**
+ * Renders a link to a form.
+ *
+ * @param {Object} props - The component props.
+ * @param {Object} props.form - The form object.
+ * @param {string} props.action - The action for the form.
+ * @param {string} props.text - The text to display for the link.
+ * @returns {JSX.Element} The rendered link component.
+ */
 export const FormLink = ({form, action, text}) => {
     return (
         <ProxyLink to={"/form/" + action + "/" + form.id}>{text}</ProxyLink>
